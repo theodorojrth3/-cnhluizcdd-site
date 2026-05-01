8d74d70150ce49e36700d2cf28f6be8a2bdcbf61
theoderojr3 <theoderojr3@users.noreply.github.com>
2026-04-30 23:09:39 -0300

    Add React + Vite app with proper gitignore

---
+++ cnhluizcdd-main/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="app">
+      <h1>CNH Digital</h1>
+      <p>Bem-vindo ao site da CNH Digital</p>
+    </div>
+  )
+}
+
+export default App
